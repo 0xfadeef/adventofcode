cc50ee9d3f76eea5a11fc4f1f30afb46f0e5d33b
0xfadeef <30846958+0xfadeef@users.noreply.github.com>
2023-01-23 02:25:49 -0500

    2015: Day 2, part 1
    https://adventofcode.com/2015/day/2

---
+++ 2015/day2.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func min(nums ...int) int {
+	var m int
+	for i, n := range(nums) {
+		if n < m || i == 0 {
+			m = n
+		}
+	}
+	return m
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Printf("missing input file\n")
+		return
+	}
+
+	input := os.Args[1]
+	f, err := os.Open(input)
+
+	if err != nil {
+		fmt.Printf("error: %v\n", err)
+		return
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	const pattern = "%dx%dx%d"
+
+	var l, w, h int
+	total := 0
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		_, err := fmt.Sscanf(line, pattern, &l, &w, &h)
+
+		if err != nil {
+			fmt.Printf("error: %v\n", err)
+			return
+		}
+		s1, s2, s3 := l * w, w * h, h * l
+		total += 2 * (s1 + s2 + s3) + min(s1, s2, s3)
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Printf("error: %v\n", err)
+	} else {
+		fmt.Println(total)
+	}
+}
