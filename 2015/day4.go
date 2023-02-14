404ff7e612bafa1497b610331ff15d7c8c71aac7
0xfadeef <30846958+0xfadeef@users.noreply.github.com>
2023-02-14 14:58:19 -0500

    2015: Day 4, part 1

---
+++ 2015/day4.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"crypto/md5"
+	"fmt"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Printf("missing input!\n")
+		return
+	}
+	secret := os.Args[1]
+	secret_length := len(secret)
+	buffer_length := secret_length + 10  // max digits in unit32
+
+	buffer := make([]byte, secret_length, buffer_length)
+	copy(buffer, secret)
+
+	var number uint32 = 1
+	for {
+		data := fmt.Append(buffer, number)
+		hash := md5.Sum(data)
+		hexhash := fmt.Sprintf("%x", hash)
+
+		if hexhash[:5] == "00000" {
+			fmt.Println(number)
+			break
+		}
+		number += 1
+	}
+}
