32fbc87765bb18ed5919aae899777987df6d7036
0xfadeef <30846958+0xfadeef@users.noreply.github.com>
2023-07-22 01:29:21 -0400

    2015: Day 10: Refactoring

--- 2015/day10.go
+++ 2015/day10.go
@@ -8,17 +8,42 @@ import (
 
 const repeats = 40
 
+type Chain []int
+
+func (chain Chain) print_total_length() {
+	result := 0
+	for _, n := range chain {
+		result += len(Elements[n].Sequence)
+	}
+	fmt.Println(result)
+}
+
+func (chain Chain) next_iteration() Chain {
+	l := len(chain)
+	for _, n := range chain {
+		chain = append(chain, Elements[n].Products...)
+	}
+	return chain[l:]
+}
+
+func (chain Chain) evolve(repeats int) Chain {
+	for i := 0; i < repeats; i++ {
+		chain = chain.next_iteration()
+	}
+	return chain
+}
+
 func main() {
 	if len(os.Args) < 2 {
 		fmt.Printf("missing input!\n")
 		return
 	}
 	input := os.Args[1]
-	var chain []int
+	var chain Chain
 
 	for _, elem := range Elements {
 		if input == elem.Sequence {
-			chain = append(chain, elem.Number)
+			chain = Chain([]int{elem.Number})
 			break
 		}
 	}
@@ -28,18 +53,7 @@ func main() {
 		return
 	}
 
-	for i := 0; i < repeats; i++ {
-		l := len(chain)
-		for _, n := range chain {
-			chain = append(chain, Elements[n].Products...)
-		}
-		chain = chain[l:]
-	}
-
-	var result = 0
-	for _, n := range chain {
-		result += len(Elements[n].Sequence)
-	}
-	fmt.Println(result)
+	chain = chain.evolve(repeats)
+	chain.print_total_length()
 }
 
