a6fcd6cc483548fb552e0ef3bd406636e95f7e80
0xfadeef <30846958+0xfadeef@users.noreply.github.com>
2024-07-05 10:10:44 -0400

    2015: Day 14: Map reindeers stats to run state

--- 2015/day14.go
+++ 2015/day14.go
@@ -13,6 +13,9 @@ type Reindeer struct {
 	name string
 	speed, stamina, cooldown int
 }
+type State struct {
+	points, distance int
+}
 
 func main() {
 	if len(os.Args) < 2 {
@@ -31,21 +34,21 @@ func main() {
 	const pattern = "%s can fly %d km/s for %d seconds, but then must rest for %d seconds."
 	scanner := bufio.NewScanner(f)
 
-	var reindeers = make([]Reindeer, 0, 32)
+	var reindeers = make(map[Reindeer]*State)
 	var max_distance = 0
 
 	for scanner.Scan() {
 		line := scanner.Text()
-		r := new(Reindeer)
+		r := Reindeer{}
 		fmt.Sscanf(line, pattern, &r.name, &r.speed, &r.stamina, &r.cooldown)
-		reindeers = append(reindeers, *r)
+		reindeers[r] = new(State)
 	}
 	if err := scanner.Err(); err != nil {
 		fmt.Printf("error: %v\n", err)
 		return
 	}
 
-	for _, r := range reindeers {
+	for r, _ := range reindeers {
 		cycle := r.stamina + r.cooldown
 		n_cycles, time_left := duration / cycle, duration % cycle
 		d := (n_cycles * r.stamina + min(time_left, r.stamina)) * r.speed
